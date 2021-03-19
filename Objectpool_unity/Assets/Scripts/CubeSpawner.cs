b59bf7f5f1d714b0234ea281a9e69e7ba22af802
WislonLin <p96091123@gs.ncku.edu.tw>
2021-03-19 18:25:49 +0800

    Version_01

---
+++ Objectpool_unity/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+
+
+    ObjectPooler objectPooler;
+
+    private void Start()
+    {
+        objectPooler = ObjectPooler.Instance;
+
+		InvokeRepeating ("Ins",1,1);
+    }
+
+	void Ins()
+	{
+		objectPooler.SpawnFromPool("Cube",this.transform.position,this.transform.rotation);
+	}
+
+   
+}
